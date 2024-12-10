f69a6f9605f40fd87daea857158e18031c69b86c
JaydethSp01 <jsimarrapolo@gmail.com>
2024-12-09 19:37:42 -0500

    Contraseña encriptada

--- GestionGastosBackend/src/main/java/com/sanmardan/GestionGastosBackend/ServicesImpl/UsuarioServiceImpl.java
+++ GestionGastosBackend/src/main/java/com/sanmardan/GestionGastosBackend/ServicesImpl/UsuarioServiceImpl.java
@@ -3,6 +3,7 @@ package com.sanmardan.GestionGastosBackend.ServicesImpl;
 import com.sanmardan.GestionGastosBackend.Model.Usuario;
 import com.sanmardan.GestionGastosBackend.Repository.UsuarioRepository;
 import com.sanmardan.GestionGastosBackend.Services.UsuarioService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,9 +12,11 @@ import java.util.List;
 public class UsuarioServiceImpl implements UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
-    public UsuarioServiceImpl(UsuarioRepository usuarioRepository) {
+    public UsuarioServiceImpl(UsuarioRepository usuarioRepository, BCryptPasswordEncoder passwordEncoder) {
         this.usuarioRepository = usuarioRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -29,15 +32,21 @@ public class UsuarioServiceImpl implements UsuarioService {
 
     @Override
     public Usuario crearUsuario(Usuario usuario) {
+        usuario.setContraseña(passwordEncoder.encode(usuario.getContraseña()));
         return usuarioRepository.save(usuario);
     }
 
     @Override
     public Usuario actualizarUsuario(int id, Usuario usuario) {
         Usuario usuarioExistente = buscarUsuarioPorId(id);
+
         usuarioExistente.setNombre(usuario.getNombre());
         usuarioExistente.setEmail(usuario.getEmail());
-        usuarioExistente.setContraseña(usuario.getContraseña());
+
+        if (usuario.getContraseña() != null && !usuario.getContraseña().isEmpty()) {
+            usuarioExistente.setContraseña(passwordEncoder.encode(usuario.getContraseña()));
+        }
+
         return usuarioRepository.save(usuarioExistente);
     }
 
